8d812dd15c7caedd50e2db8bfe73cf6a107912e9
Artyom Tugaryov <artyom.tugaryov@gmail.com>
2023-07-02 16:24:01 +0300

    enable sample

--- cli/sample.py
+++ cli/sample.py
@@ -1,3 +1,5 @@
 from dobblebot.services.yolov5 import YoloService
 
-service = YoloService('D:\Development\dobble-org\dobblebot\data\dobblenet_yolov5m.pt')
+
+if __name__ == '__main__':
+    service = YoloService('D:\Development\dobble-org\dobblebot\data\dobblenet_yolov5m.pt')
